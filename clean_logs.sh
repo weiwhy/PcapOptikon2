471a9c76fece585b0e62cba8d91cdf12437e87af
Federico Foschini <ffoschini@certego.net>
2020-05-05 10:18:48 +0200

    Added clean_logs script

---
+++ clean_logs.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Get the full path of this script so we can access the correct log directories
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+SURICATA_LOGS="${DIR}/logs/suricata/eve.json"
+ZEEK_LOGS="${DIR}/logs/zeek/*.log"
+echo "Deleting ${SURICATA_LOGS}"
+rm -f ${SURICATA_LOGS}
+echo "Deleting ${ZEEK_LOGS}"
+rm -f ${ZEEK_LOGS}
+
